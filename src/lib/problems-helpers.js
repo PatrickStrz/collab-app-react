e4f5f1b1d39b83afb673996234ac8c70a7fd4abd
PatrickStrz <patrick.strzelec@gmail.com>
2017-03-06 18:22:55 -0500

    added more helper functions and tests for problem local state

--- src/lib/problems-helpers.js
+++ src/lib/problems-helpers.js
@@ -1,8 +1,30 @@
 
-export const addProblemToState = (problemId,problems)=>{
+export const addProblem = (problemId,problems)=>{
 
-const problem = {id:problemId, ideasListToggle:false}
+  const problem = {id:problemId, ideasVisible:false}
 
-return [...problems, problem]
+  return [...problems, problem]
+
+}
+
+export const toggleIdeasForProblem = (problemId,problems)=>{
+
+  const problemIndex = problems.findIndex(problem => problem.id === problemId)
+
+  const problem = problems[problemIndex]
+
+  problem.ideasVisible = !problem.ideasVisible
+
+  return [...problems.slice(0,problemIndex), problem ,...problems.slice(problemIndex+1)]
+
+}
+
+export const removeProblem = (problemId,problems)=>{
+
+  const removeIndex = problems.findIndex(problem => problem.id === problemId)
+  console.log(removeIndex)
+
+  return [...problems.slice(0,removeIndex),...problems.slice(removeIndex+1)
+  ]
 
 }
--- src/lib/problems-helpers.test.js
+++ src/lib/problems-helpers.test.js
@@ -1,36 +1,107 @@
 
-import {addProblemToState} from './problems-helpers'
+import {addProblem, toggleIdeasForProblem, removeProblem} from './problems-helpers'
 
-test('addProblemToState should should add problem ui state object to the existing problem array', ()=>{
+test('addProblem should add problem object to array', ()=>{
   const startProblems = [
-    {id:1, ideasListToggle:false}
+    {id:1, ideasVisible:false}
   ]
   const problemId= 4
-
-  const result = addProblemToState(problemId, startProblems)
+  const result = addProblem(problemId, startProblems)
 
   const expected = [
-    {id:1, ideasListToggle:false},
-    {id:4, ideasListToggle:false}
+    {id:1, ideasVisible:false},
+    {id:4, ideasVisible:false}
   ]
 
   expect(result).toEqual(expected)
-
 })
 
-test('addProblemToUiState should not mutate the existing problem array', ()=>{
+test('addProblem should not mutate the existing problem array', ()=>{
   const startProblems = [
-    {id:1, ideasListToggle:false}
+    {id:1, ideasVisible:false}
   ]
   const problemId= 4
-
-  const result = addProblemToState(problemId, startProblems)
+  const result = addProblem(problemId, startProblems)
 
   const expected = [
-    {id:1, ideasListToggle:false},
-    {id:4, ideasListToggle:false}
+    {id:1, ideasVisible:false},
+    {id:4, ideasVisible:false}
   ]
 
   expect(result).not.toBe(expected)
+})
 
+test('toggleIdeasListForProblem should toggle ideasVisible prop of problem', ()=>{
+  const startProblems = [
+    {id:1, ideasVisible:false},
+    {id:4, ideasVisible:false},
+    {id:9, ideasVisible:false},
+    {id:35, ideasVisible:false},
+  ]
+
+  const problemId = 4
+  const result = toggleIdeasForProblem(problemId, startProblems)
+
+  const expected = [
+    {id:1, ideasVisible:false},
+    {id:4, ideasVisible:true},
+    {id:9, ideasVisible:false},
+    {id:35, ideasVisible:false},
+  ]
+
+  expect(result).toEqual(expected)
+})
+
+test('toggleIdeasListForProblem should not mutate existing list', ()=>{
+  const startProblems = [
+    {id:1, ideasVisible:false},
+    {id:4, ideasVisible:false},
+    {id:9, ideasVisible:false},
+    {id:35, ideasVisible:false},
+  ]
+  const problemId = 4
+  const result = toggleIdeasForProblem(problemId, startProblems)
+
+  const expected = [
+    {id:1, ideasVisible:false},
+    {id:4, ideasVisible:true},
+    {id:9, ideasVisible:false},
+    {id:35, ideasVisible:false},
+  ]
+
+  expect(result).not.toBe(expected)
+})
+
+test('removeProblem should remove a problem by id', ()=>{
+  const startProblems = [
+    {id:1, ideasVisible:false},
+    {id:4, ideasVisible:false},
+    {id:9, ideasVisible:false},
+  ]
+  const problemId = 4
+  const result = removeProblem(problemId, startProblems)
+
+  const expected = [
+    {id:1, ideasVisible:false},
+    {id:9, ideasVisible:false},
+  ]
+
+  expect(result).toEqual(expected)
+})
+
+test('removeProblem should not mutate the existing array', ()=>{
+  const startProblems = [
+    {id:1, ideasVisible:false},
+    {id:4, ideasVisible:false},
+    {id:9, ideasVisible:false},
+  ]
+  const problemId = 4
+  const result = removeProblem(problemId, startProblems)
+
+  const expected = [
+    {id:1, ideasVisible:false},
+    {id:9, ideasVisible:false},
+  ]
+
+  expect(result).not.toBe(expected)
 })
