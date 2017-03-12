7348ff5da76eabdec0a1fef91976443bb6440a2d
Patrick Strzelec <PatrickStrz@users.noreply.github.com>
2017-03-12 00:46:30 -0500

    changed base url (#4)

--- src/actions/api-commons.js
+++ src/actions/api-commons.js
@@ -1,3 +1,3 @@
 export const BASE_URL_PRODUCTION = "https://collab-app-2017.herokuapp.com/"
 export const BASE_URL_LOCAL = "http://localhost:3000"
-export const BASE_URL = BASE_URL_LOCAL
+export const BASE_URL = BASE_URL_PRODUCTION
